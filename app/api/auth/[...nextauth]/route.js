766bcb722b43a565fbb03386070d4250a320f15c
harshit0571 <harshitchadha65@gmail.com>
2023-06-26 14:23:44 +0530

    added authentication

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+const handler = new NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+});
+export { handler as GET, handler as POST };
---
+++ app/page.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { signIn, getProviders, signOut, useSession } from "next-auth/react";
+
+export default function Home() {
+  const { data: session } = useSession();
+  const [Providers, setProviders] = useState(null);
+  useEffect(() => {
+    const getP = async () => {
+      const response = await getProviders();
+      setProviders(response);
+      console.log(response);
+    };
+    getP();
+  }, []);
+
+  return (
+    <main className="bg-blue-900 w-screen h-screen flex flex-col justify-center items-center gap-10">
+      {Providers &&
+        Object.values(Providers).map((provider) => (
+          <div key={provider.name}>
+            <button
+              className="bg-white text-blue-600 p-2 px-4 transition-colors shadow-white text-lg rounded-md hover:bg-blue-400 hover:text-white"
+              onClick={() => {
+                signIn(provider.id);
+              }}
+            >
+              Login With {provider.name}
+            </button>
+          </div>
+        ))}
+    </main>
+  );
+}
+
+{
+  /* <div>
+<button className="bg-white text-black p-2 px-4 transition-colors shadow-white text-lg rounded-md hover:bg-black hover:text-white">
+  Login With Github
+</button>
+</div> */
+}
