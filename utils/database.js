2774228d96990cdc0c815108d38fa662b016b08d
harshit0571 <harshitchadha65@gmail.com>
2023-06-26 14:37:53 +0530

    database util added, connectToDB()

---
+++ utils/database.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+  if (isConnected) {
+    console.log("is connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share-prompt",
+      useNewUrlParser: "true",
+      useUnifiedTopology: "true",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
